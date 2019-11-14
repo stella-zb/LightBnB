4d48ef52b2e77628a944f24a02d1ceae01b5041b
stella-zb <upperdaystudio@gmail.com>
2019-11-14 02:25:08 +0000

    correct typo error

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -39,7 +39,7 @@ CREATE TABLE reservations (
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  guess_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
@@ -56,7 +56,7 @@ CREATE TABLE rates (
 
 CREATE TABLE guess_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  guess_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
