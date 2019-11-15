f050b0d2f8e1a503e95d1495f61f24da99606b8d
stella-zb <upperdaystudio@gmail.com>
2019-11-15 04:13:47 +0000

    refactor with sql to showing all reservations for loginIn user

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -113,7 +113,25 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+    SELECT properties.*, reservations.*, AVG(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < Now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `, [guest_id,limit])
+  .then(res => {
+    if (res.rows) {
+      return res.rows;
+    } else {
+      return null;
+    }
+  })
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
